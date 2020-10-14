f7a6486455cbb54dec093b004719b34b68bb4255
Samir Ortiz <samirortiz@gmail.com>
2020-10-14 19:19:12 -0300

    Unit tests and application core

--- index.php
+++ index.php
@@ -4,3 +4,9 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
     require __DIR__ . '/vendor/autoload.php';
 }
 
+use App\Controller\ApiController;
+use App\Service\CurrencyService;
+use App\Http\Response\JsonResponse;
+
+$controller = new ApiController(new CurrencyService, new JsonResponse);
+$controller->personareApi();
